92ce303f799aa7292b7cda6d6d41c7bbc04dcac0
Tim W <timrlw@gmail.com>
2018-08-20 18:31:55 +0800

    fix

--- app/src/main/java/com/msfdroid/model/Defaults.java
+++ app/src/main/java/com/msfdroid/model/Defaults.java
@@ -6,6 +6,7 @@ public class Defaults {
     public static final String DEFAULT_USER = "msf";
     public static final String DEFAULT_PORT = "55553";
     public static final String DEFAULT_PASSWORD = "";
+    public static final boolean DEFAULT_SSL = false;
     public static final String DEFAULT_GCM_SENDER = "";
     public static final String DEFAULT_GCM_API_KEY = "";
 
